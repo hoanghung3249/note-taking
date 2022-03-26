76ac41e457d19d16b487b51d820cad974cd24fa7
Hung Nguyen <nguyenhung5890@gmail.com>
2022-03-26 11:10:48 +0700

    init proj

---
+++ NoteTaking/NoteTaking/NoteTakingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteTakingApp.swift
+//  NoteTaking
+//
+//  Created by Hung Nguyen on 26/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct NoteTakingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
