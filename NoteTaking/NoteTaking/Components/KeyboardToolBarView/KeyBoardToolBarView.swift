ea07bf6b6e9d2a6ae4016d868f300778a7d9ad26
Hung Nguyen <nguyenhung5890@gmail.com>
2022-06-18 10:35:22 +0700

    Optimize keyboard toolbar animation

--- NoteTaking/NoteTaking/Components/KeyboardToolBarView/KeyBoardToolBarView.swift
+++ NoteTaking/NoteTaking/Components/KeyboardToolBarView/KeyBoardToolBarView.swift
@@ -12,11 +12,13 @@ final class KeyboardResponder: ObservableObject {
     
     private var notificationCenter: NotificationCenter
     @Published private(set) var currentHeight: CGFloat = 0
+    @Published private(set) var visible: Bool = false
 
     init(center: NotificationCenter = .default) {
         notificationCenter = center
         notificationCenter.addObserver(self, selector: #selector(keyBoardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         notificationCenter.addObserver(self, selector: #selector(keyBoardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(keyBoardDidShow(notification:)), name: UIResponder.keyboardDidShowNotification, object: nil)
     }
 
     func dismiss() {
@@ -40,6 +42,16 @@ final class KeyboardResponder: ObservableObject {
         withAnimation { [weak self] in
             guard let self = self else { return }
             self.currentHeight = 0
+            self.visible = false
+        }
+    }
+    
+    @objc func keyBoardDidShow(notification: Notification) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) { [weak self] in
+            guard let self = self else { return }
+            withAnimation(.spring()) {
+                self.visible = true
+            }
         }
     }
 }
@@ -54,16 +66,17 @@ struct KeyboardView<Content, ToolBar> : View where Content : View, ToolBar: View
     var body: some View {
         ZStack {
             content()
-                .padding(.bottom, (keyboard.currentHeight == 0) ? 0 : toolbarFrame.height)
+                .padding(.bottom, keyboard.visible ? toolbarFrame.height : 0)
             VStack {
                  Spacer()
                  toolBar()
                     .frame(width: toolbarFrame.width, height: toolbarFrame.height)
                     .background(Color.whiteLilac)
+                    .opacity(keyboard.visible ? 1 : 0)
                     .hidden(!isShowToolBar)
                     .cornerRadius(5, corners: [.topLeft, .topRight])
             }
-            .opacity((keyboard.currentHeight == 0) ? 0 : 1)
+            .opacity(keyboard.visible ? 1 : 0)
             .animation(.easeOut)
         }
         .padding(.bottom, keyboard.currentHeight)
